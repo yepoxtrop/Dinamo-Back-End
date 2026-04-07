a60c6e393b1e414c0d2543fe4987719f18bfdab7
yepoxtrop <sarmientodiazluisangel@gmail.com>
2026-04-07 15:52:08 -0500

    feat: se agrega controlador para actualizar el correo del usuario dentro de la base de datos de Dinamo

--- Apis/Api_Js/src/modules/baseDatos/prisma/consultas/correos.js
+++ Apis/Api_Js/src/modules/baseDatos/prisma/consultas/correos.js
@@ -1,5 +1,17 @@
 import clientePrisma from "../../../../settings/prisma/clientePrisma.js";
 
+/**
+ * Actualiza el correo electrónico de un usuario en la base de datos.
+ * @author Luis Angel Sarmiento Diaz
+ * @param {Object} params - Parámetros para la actualización.
+ * @param {number} params.usuarioId - ID del usuario cuyo correo se va a actualizar.
+ * @param {string} params.correoNuevo - Nuevo correo electrónico del usuario.
+ * @description Esta función actualiza el campo usuario_correo en la tabla usuarios de la base de datos
+ * utilizando Prisma Client. Realiza una operación de actualización (update) en el registro del usuario
+ * especificado por usuarioId.
+ * @returns {Promise<Object>} - Devuelve el resultado de la operación de actualización de Prisma.
+ * @throws {Error} - Lanza un error si ocurre un problema durante la actualización en la base de datos.
+ */
 export const actualizarCorreo = async({usuarioId, correoNuevo}) =>{
     console.log(usuarioId, correoNuevo);
     
